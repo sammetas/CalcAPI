c90ea6ec2750890eb0e6b1037e76fe6336365d34
“nagasrinivas.sammeta” <nagasrinivas.sammeta@siriustech.io>
2024-01-12 11:57:40 +0700

    calc:essilar1

--- src/main/java/com/example/samm/CalcAPI/controller/CalculatorAPI.java
+++ src/main/java/com/example/samm/CalcAPI/controller/CalculatorAPI.java
@@ -3,10 +3,7 @@ package com.example.samm.CalcAPI.controller;
 
 import com.example.samm.CalcAPI.CalculatorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -22,6 +19,26 @@ public class CalculatorAPI {
 
     @GetMapping("/calc")
     public ResponseEntity<String> getCalcResult(@RequestBody String expression) {
+
+        if (Objects.isNull(expression) || expression == "") {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(service.calculate(expression));
+    }
+
+    @GetMapping("/calc1")
+    public ResponseEntity<String> getCalcResult1(@RequestParam  String expression) {
+
+        if (Objects.isNull(expression) || expression == "") {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(service.calculate(expression));
+    }
+
+    @GetMapping("/calc2/{expression}")
+    public ResponseEntity<String> getCalcResult2(@PathVariable  String expression) {
 
         if (Objects.isNull(expression) || expression == "") {
             return ResponseEntity.badRequest().build();
